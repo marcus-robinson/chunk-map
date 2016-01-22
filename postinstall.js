60f4b8109e72744293d1ce52fa867325df418d9e
slamborne <shanelamborne@gmail.com>
2016-01-22 18:23:26 -0500

    removed stray file

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-var fse = require("fs-extra");
-var path = require("path");
-
-try {
-  fse.copySync(path.join(process.cwd(), "./node_modules/phaser/dist/"), path.join(process.cwd(), "./www/lib/phaser/"));
-} catch(e) {
-  throw e;
-}
